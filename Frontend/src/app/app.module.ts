dd2e91c61f3ec15e916e90c37156a7f631db4e02
John Harvey Gomez Vega <jgomez@gravitalsoftware.com>
2017-08-15 13:40:15 -0500

    cards para la informacion

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { ChoiceProductComponent } from './student/choice-product/choice-product.component';
+import { InformationComponent } from './student/information/information.component';
 import { PresentationComponent } from './student/presentation/presentation.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MdTabsModule } from '@angular/material';
+import { MdTabsModule, MdCardModule, MdGridListModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -12,6 +14,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     PresentationComponent,
+    InformationComponent,
     ChoiceProductComponent    
   ],
   imports: [
@@ -19,6 +22,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     FormsModule,
     HttpModule,
     MdTabsModule,
+    MdCardModule,
+    MdGridListModule,
     BrowserAnimationsModule
   ],
   providers: [],
